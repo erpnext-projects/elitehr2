295a57fd6baa1c5a3ef6c9f66db84dd8f4d2f7a7
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2026-03-09 15:58:38 +0000

    تحديد موقعي علي الخريطة

--- elitehr2/elitehr2/doctype/elitehr_fingerprint_sites/elitehr_fingerprint_sites.js
+++ elitehr2/elitehr2/doctype/elitehr_fingerprint_sites/elitehr_fingerprint_sites.js
@@ -4,9 +4,45 @@
 frappe.ui.form.on("Elitehr Fingerprint Sites", {
 	refresh(frm) {
         render_radius_slider(frm);
+        drawMap(frm);
 	},
+    find_my_current_location:function(frm) {
+        getCurrentLocation(frm);
+    }
 });
 
+
+
+
+
+function getCurrentLocation(frm) {
+    if (!navigator.geolocation) {
+            frappe.msgprint("المتصفح لا يدعم تحديد الموقع");
+            return;
+        }
+        navigator.geolocation.getCurrentPosition(function(position) {
+
+            let lat = position.coords.latitude;
+            let lng = position.coords.longitude;
+
+            frm.set_value("latitude", lat);
+            frm.set_value("longitude", lng);
+
+            if (frm.map) {
+                frm.map.setView([lat, lng], 16);
+
+                if (frm.marker) {
+                    frm.marker.setLatLng([lat, lng]);
+                } else {
+                    frm.marker = L.marker([lat, lng]).addTo(frm.map);
+                }
+            }
+
+        }, function(error){
+            frappe.msgprint("لم يتم الحصول على الموقع" + " " + error.message);
+            
+        });
+}
 
 function render_radius_slider(frm) {
 
@@ -62,4 +98,49 @@ function render_radius_slider(frm) {
                     __("Employees can check-in if they are within {0} meters from this location", [val])
                 );
         });
+}
+
+function drawMap(frm) {
+
+    if (frm.map) {
+        return;
+    }
+    frm.fields_dict.map_html.$wrapper.html(`<div id="map" style="height:400px"></div>`);
+
+    frm.map = L.map('map').setView([30.0444, 31.2357], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'OpenStreetMap'
+    }).addTo(frm.map);
+
+    frm.map.on('click', function(e) {
+
+        let lat = e.latlng.lat;
+        let lng = e.latlng.lng;
+
+        frm.set_value("latitude", lat);
+        frm.set_value("longitude", lng);
+
+        set_marker(frm, lat, lng);
+
+    });
+
+    // 👇 رسم الماركر بعد الحفظ
+    if (frm.doc.latitude && frm.doc.longitude) {
+
+        set_marker(frm, frm.doc.latitude, frm.doc.longitude);
+
+        frm.map.setView([frm.doc.latitude, frm.doc.longitude], 16);
+    }
+}
+
+
+function set_marker(frm, lat, lng) {
+
+    if (frm.marker) {
+        frm.marker.setLatLng([lat, lng]);
+    } else {
+        frm.marker = L.marker([lat, lng]).addTo(frm.map);
+    }
+
 }
