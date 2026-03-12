f55f141fb8a8f4d3681f62b06ca6a766e0d0d0cc
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2026-03-12 08:46:35 +0000

    سلاسل الموفقات

--- elitehr2/elitehr2/doctype/elitehr_requests/elitehr_requests.js
+++ elitehr2/elitehr2/doctype/elitehr_requests/elitehr_requests.js
@@ -17,5 +17,41 @@ frappe.ui.form.on("Elitehr Requests", {
                 },
             };
         });
+
+        setupSubmitBtn(frm);
 	},
 });
+
+function setupSubmitBtn(frm) {
+     // let leaves_ids = frm.doc.table_leaves.map(l => l.leave);
+        // emptyLevelStatus = list(filter(lambda l: l.status is None, self.levels))
+        // if len(emptyLevelStatus)  != len(self.levels):
+
+        let emptyLevelStatus = frm.doc.levels.filter(l => l.status === undefined);
+        if(emptyLevelStatus.length > 0){
+            frm.add_custom_button(__("Submit"), function() {
+                frappe.prompt([
+                    {
+                        label: 'Status',
+                        fieldname: 'status',
+                        fieldtype: 'Select',
+                        options: ["Rejected","Approve","Under Liquidation","Disbursement of Dues"],
+                        reqd: 1
+                    }
+                ], (values) => {
+                    frappe.call({
+                        method: "elitehr2.elitehr2.doctype.elitehr_requests.elitehr_requests.update_approval",
+                        args: {
+                            docname: frm.doc.name,
+                            status: values.status
+                        },
+                        callback: function() {
+                            frm.reload_doc();
+                        }
+                    });
+                })
+    
+    
+            });   
+        }
+}
--- elitehr2/elitehr2/doctype/elitehr_requests/elitehr_requests.py
+++ elitehr2/elitehr2/doctype/elitehr_requests/elitehr_requests.py
@@ -7,39 +7,50 @@ import frappe
 
 class ElitehrRequests(Document):
     def before_save(self):
-        workflow = frappe.get_last_doc(
-            'Elitehr Approval Workflow',
-            filters={'active': '1', "request_type": self.type}
-        ) if frappe.db.exists('Elitehr Approval Workflow', {'active': '1', "request_type": self.type}) else None
-        
-        if workflow is None:
-            frappe.throw(f"No active approval workflow found for request type: {self.type}")
-        
-        departmentId = frappe.get_doc("Elitehr Employee",self.employee).department
-        departmentManager = frappe.get_doc("Elitehr Fingerprint Sites",departmentId).manager
-        manager = frappe.get_doc("Elitehr Employee",departmentManager)
-
-        directSupervisor = frappe.get_doc('Elitehr Employee', self.employee).manager
-        directSupervisorName = frappe.get_doc('Elitehr Employee', directSupervisor).employee_name
-
-        self.levels = []
-        for level in workflow.levels:
-            approvedType = level.approved_type
-            responsible = None
+        # Check if all levels have been approved
+        emptyLevelStatus = list(filter(lambda l: l.status is None, self.levels))
+        # if all is empty
+        if len(emptyLevelStatus)  == len(self.levels):
+            workflow = frappe.get_last_doc(
+                'Elitehr Approval Workflow',
+                filters={'active': '1', "request_type": self.type}
+            ) if frappe.db.exists('Elitehr Approval Workflow', {'active': '1', "request_type": self.type}) else None
             
-            if approvedType == "Specific Employee":
-                responsible = frappe.get_doc('Elitehr Employee', level.employee).employee_name
-            elif approvedType == "Department Manager":
-                responsible = manager.employee_name
-            elif approvedType == "Direct Supervisor":
-                responsible = directSupervisorName
+            if workflow is None:
+                frappe.throw(f"No active approval workflow found for request type: {self.type}")
             
-            frappe.log(approvedType)
+            department = frappe.get_doc("Elitehr Employee",self.employee)
+            departmentId = department.department
+            departmentName = department.department_name
+            departmentManager = frappe.get_doc("Elitehr Fingerprint Sites",departmentId).manager
+            if departmentManager is None:
+                frappe.throw(f"لم يتم العثور علي مدير الفرع {departmentName}")
 
-            self.append('levels', {
-                    'approved_type': approvedType,
-                    "responsible": responsible
-            })
+            manager = frappe.get_doc("Elitehr Employee",departmentManager)
+
+            directSupervisor = frappe.get_doc('Elitehr Employee', self.employee).manager
+            
+            directSupervisorName = frappe.get_doc('Elitehr Employee', directSupervisor).employee_name
+
+            self.levels = []
+            for level in workflow.levels:
+                approvedType = level.approved_type
+                responsible = None
+                
+                if approvedType == "Specific Employee":
+                    responsible = frappe.get_doc('Elitehr Employee', level.employee).employee_name
+                elif approvedType == "Department Manager":
+                    responsible = manager.employee_name
+                elif approvedType == "Direct Supervisor":
+                    responsible = directSupervisorName
+                
+                # frappe.log(approvedType)
+
+                self.append('levels', {
+                        'approved_type': approvedType,
+                        "responsible": responsible
+                })
+
 
 
 
@@ -51,3 +62,19 @@ def get_requests_list(request_type):
         fields=["*"]
     )
     return requests
+
+
+@frappe.whitelist()
+def update_approval(docname, status):
+    doc = frappe.get_doc("Elitehr Requests", docname)
+    frappe.log(doc.levels)
+    for row in doc.levels:
+        frappe.log(f"row.status {row.status}")
+        # if row.approver == frappe.session.user and row.status == "Pending":
+        if row.status is None:
+            row.status = status
+            row.action_date = frappe.utils.now()
+            doc.status = status
+            doc.save() 
+            frappe.db.commit()
+            break
