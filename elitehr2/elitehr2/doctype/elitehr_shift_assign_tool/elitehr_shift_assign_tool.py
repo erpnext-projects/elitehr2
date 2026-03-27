6187d19fb3b045b6a06bee1dfea937f22f0d881d
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2026-03-27 19:17:17 +0000

    تعيين الشيفت

--- elitehr2/elitehr2/doctype/elitehr_shift_assign_tool/elitehr_shift_assign_tool.py
+++ elitehr2/elitehr2/doctype/elitehr_shift_assign_tool/elitehr_shift_assign_tool.py
@@ -1,9 +1,36 @@
 # Copyright (c) 2026, Mohamed Elgohary and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ElitehrShiftAssignTool(Document):
-	pass
+    def before_save(self):
+        to = self.appointment_for
+        if to == "Employee":
+            expectEmployees = [emp.employee for emp in self.employees]
+            employees = frappe.get_all("Elitehr Employee",pluck="name",filters={"name": ["in", expectEmployees]})            
+            self.assignShift(employees)
+        
+        elif to == "Department":
+            employees = frappe.get_all("Elitehr Employee",pluck="name",filters={"department": self.department})
+            self.assignShift(employees)
+
+
+
+    def assignShift(self,employees):
+        total = len(employees)
+        checked = 0
+        updated = 0
+        for emp_name in employees:
+            checked += 1
+            employee = frappe.get_doc("Elitehr Employee",emp_name)
+            employee.shift = self.shift
+            employee.save()
+            updated += 1
+
+        frappe.msgprint(
+            f"تم مراجعة {checked} موظف<br>"
+            f"تم إضافة الوردية إلى {updated} موظف"
+        )
