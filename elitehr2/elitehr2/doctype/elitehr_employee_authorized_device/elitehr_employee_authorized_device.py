393b2a179cb03a541ea695bf285e2b2bf39a78f4
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2026-05-13 07:26:21 +0000

    اضافة الاجهوة المعتمدة

--- elitehr2/elitehr2/doctype/elitehr_employee_authorized_device/elitehr_employee_authorized_device.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Mohamed Elgohary and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ElitehrEmployeeAuthorizedDevice(Document):
-	pass
