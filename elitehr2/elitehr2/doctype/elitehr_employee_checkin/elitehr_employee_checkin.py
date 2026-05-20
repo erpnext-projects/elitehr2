69102e1c035727d72075356b6d850bfe2720bb47
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2026-05-20 11:09:23 +0000

    الاعتماد علي الموظف النشط فقط

--- elitehr2/elitehr2/doctype/elitehr_employee_checkin/elitehr_employee_checkin.py
+++ elitehr2/elitehr2/doctype/elitehr_employee_checkin/elitehr_employee_checkin.py
@@ -329,6 +329,7 @@ def get_employee_attendance_handler(employee=None,from_date=None,to_date=None):
     if employee is None:
         employees = frappe.get_all(
             "Elitehr Employee",
+            filters={"status": "Active"},
             fields=["name", "employee_name", "department","department_name","shift","job_title"]
         )
     else:
