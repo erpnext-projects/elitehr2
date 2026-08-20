8336f8955f4ee6ac72461480386594b354dbe14f
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2026-08-20 14:07:00 +0000

    solve error create request

--- elitehr2/elitehr2/doctype/elitehr_requests/elitehr_requests.py
+++ elitehr2/elitehr2/doctype/elitehr_requests/elitehr_requests.py
@@ -23,7 +23,7 @@ class ElitehrRequests(Document):
         # check if request is add authorized device and its first one
         r = frappe.get_all("Elitehr Requests", filters={"employee": self.employee, "type": "ADD_AUTHORIZED_DEVICE"}, fields=["name"])
         if self.type == "ADD_AUTHORIZED_DEVICE" and len(r) == 0:
-            self.status = "Approve"
+            self.status = "Approved"
 
 
         
