299a5b3c84eb015cbbf0c724e08a9cb4e90ab515
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2026-08-20 19:53:00 +0000

    مرتب العمالة الموسمية

--- elitehr2/elitehr2/doctype/elitehr_supply_contract_payroll/elitehr_supply_contract_payroll.js
+++ elitehr2/elitehr2/doctype/elitehr_supply_contract_payroll/elitehr_supply_contract_payroll.js
@@ -4,64 +4,41 @@ let tableContainer = $(`<div class="table_container"></div>`);
 
 frappe.ui.form.on("Elitehr Supply Contract Payroll", {
 	refresh(frm) {
-        calculate(frm);
+        // calculate(frm);
+        let table_data = frm.doc.table_data;
+        if (typeof table_data === "string") {
+			try {
+				table_data = JSON.parse(table_data);
+                renderTable(frm, table_data.data);
+                
+			} catch (error) {
+				console.error("Invalid table_data JSON:", error);
+				return;
+			}
+		}
+        
 	},
     from: function(frm) {
-        calculate(frm);
+        // calculate(frm);
     },
     to: function(frm) {
-        calculate(frm);
+        // calculate(frm);
     },
-    deduction_percentage: function(frm) {
-        let val = frm.doc.deduction_percentage;
-        if (val < 0) {
-            frm.set_value("deduction_percentage", 0);
-        }
-        else if (val > 100) {
-            frm.set_value("deduction_percentage", 100);
-        }else{
-            calculate(frm);
-        }
-    },
+    // deduction_percentage: function(frm) {
+    //     let val = frm.doc.deduction_percentage;
+    //     if (val < 0) {
+    //         frm.set_value("deduction_percentage", 0);
+    //     }
+    //     else if (val > 100) {
+    //         frm.set_value("deduction_percentage", 100);
+    //     }else{
+    //         // calculate(frm);
+    //     }
+    // },
     the_contract: function(frm) {
-        calculate(frm);
+        // calculate(frm);
     }
 });
-
-function calculate(frm) {
-	let fromDate = frm.doc.from;
-	let toDate = frm.doc.to;
-
-	if (!fromDate || !toDate || !frm.doc.the_contract) {
-        tableContainer.empty();
-        return;
-    }
-
-	frappe.call({
-		method: "elitehr2.elitehr2.doctype.elitehr_supply_contract_payroll.elitehr_supply_contract_payroll.get_payroll_data",
-		args: {
-            the_contract: frm.doc.the_contract,
-            houry_wage: frm.doc.hourly_wage,
-			from_date: fromDate,
-			to_date: toDate,
-			deduction_percentage: frm.doc.deduction_percentage || 0
-		},
-		callback(r) {
-			if (!r.message) return;
-
-			let response = r.message;
-
-			renderTable(frm, response.payroll);
-
-			frm.set_value("workers", response.workers);
-			frm.set_value("total_seconds", response.total_seconds);
-			frm.set_value("hours", response.hours);
-			frm.set_value("total", response.total);
-			frm.set_value("net", response.net);
-			frm.set_value("total_deductions", response.total_deductions);
-		}
-	});
-}
 
 function renderTable(frm,data) {
     let wrapper = $(frm.fields_dict.table.wrapper);
--- elitehr2/elitehr2/doctype/elitehr_supply_contract_payroll/elitehr_supply_contract_payroll.py
+++ elitehr2/elitehr2/doctype/elitehr_supply_contract_payroll/elitehr_supply_contract_payroll.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class ElitehrSupplyContractPayroll(Document):
@@ -36,108 +37,196 @@ class ElitehrSupplyContractPayroll(Document):
 			frappe.throw(_(
 				"Payroll net amount cannot exceed contract value"
 			))
+   
+		if self.hourly_wage == 0:
+			frappe.throw(_("The selected contract does not have an hourly rate defined"))
 
 
+	def before_save(self):
+		self.calc_payroll()
 
+	def calc_payroll(self):
+     
+		contract_workers = frappe.get_all(
+			"Elitehr Supply Contract Workers",
+			filters={
+				"the_contract": self.the_contract
+			},
+			fields=["name","full_name"]
+		)
+  
+		if not contract_workers:
+			self.workers = 0
+			self.set("table_data", []) 
+			return
 
-
-@frappe.whitelist()
-def get_payroll_data(the_contract,houry_wage,from_date=None, to_date=None, deduction_percentage=0):
-	
-	if houry_wage == 0:
-		frappe.msgprint(_("The selected contract does not have an hourly rate defined"))
-		return {
-			"payroll": [],
-			"workers": 0,
-			"total_seconds": 0,
-			"hours": 0,
-			"total": 0,
-			"net": 0,
-			"total_deductions": 0
-		}
-
-	contract_workers = frappe.get_all(
-		"Elitehr Supply Contract Workers",
-		filters={
-			"the_contract": the_contract
-		},
-		fields=["name"]
-	)
-	worker_list = [d.name for d in contract_workers]
-	frappe.log(worker_list)
-
-	deduction_percentage = float(deduction_percentage or 0)
-
-	rows = frappe.get_all(
-		"Elitehr Workers Check_in_out",
-		filters={
-			"the_worker":["in",worker_list],
-			"date": ["between", [from_date, to_date]],
-		},
-		fields=["*"]
-	)
-
-	grouped = {}
-
-	for item in rows:
-		worker = item.get("the_worker")
-
-		if worker not in grouped:
-			grouped[worker] = {
-				"worker": worker,
-				"worker_name": item.get("worker_name"),
+  
+		worker_list = [d.name for d in contract_workers]
+  
+		grouped = {}
+		for cw in contract_workers:
+			grouped[cw.name] = {
+				"worker": cw.name,
+    			"worker_name": cw.get("full_name") or "",
 				"days": set(),
 				"total_seconds": 0
 			}
+      
 
-		grouped[worker]["days"].add(str(item.get("date")))
-		grouped[worker]["total_seconds"] += item.get("working_seconds") or 0
+		rows = frappe.get_all(
+				"Elitehr Workers Check_in_out",
+				filters={
+					"the_worker":["in",worker_list],
+					"date": ["between", [self.get("from"), self.get("to")]],
+					"status": "Approved"
+				},
+				fields=["*"]
+			)
+		
+  
+		for item in rows:
+			worker = item.get("the_worker")
+			if worker in grouped:
+				grouped[worker]["days"].add(str(item.get("date")))
+				grouped[worker]["total_seconds"] += flt(item.get("working_seconds"))
+			
+   
+		payroll = []
+		total_seconds = 0
+		total_total = 0
+		total_net = 0
+		total_deductions = 0
+  
+		for row in grouped.values():      
+			total_seconds += row["total_seconds"]
+	
+			hours = int(row["total_seconds"] // 3600)
+			minutes = int((row["total_seconds"] % 3600) // 60)
+	
+			working_hours = f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}"
+	
+			total_hours_decimal = row["total_seconds"] / 3600
+			# daily_wage = total_hours_decimal * float(houry_wage)
+	
+			total = total_hours_decimal * float(self.hourly_wage)
+			deduction = total * (self.deduction_percentage / 100)
+			net = total - deduction
+	
+			total_total += total
+			total_deductions += deduction
+			total_net += net
+	
+			payroll.append({
+				"worker": row["worker"],
+				"worker_name": row["worker_name"],
+				"days": len(row["days"]),
+				"working_hours": working_hours,
+				# "daily_wage": daily_wage,
+				"total": flt(total, 2),
+				"deduction": flt(deduction, 2),
+				"net": flt(net, 2)
+			})
+	
+		total_hours = int(total_seconds // 3600)
+		total_minutes = int((total_seconds % 3600) // 60)
 
-	payroll = []
-	total_seconds = 0
-	total_total = 0
-	total_net = 0
-	total_deductions = 0
+		self.workers = len(worker_list)
+		self.total_seconds = total_seconds
+		self.hours = f"{str(total_hours).zfill(2)}:{str(total_minutes).zfill(2)}"
+		self.total = flt(total_total, 2)
+		self.net = flt(total_net, 2)
+		self.total_deductions = flt(total_deductions, 2)
+		
+		# payroll
+		self.table_data = {"data": payroll}
 
-	for row in grouped.values():
-		total_seconds += row["total_seconds"]
 
-		hours = int(row["total_seconds"] // 3600)
-		minutes = int((row["total_seconds"] % 3600) // 60)
+# @frappe.whitelist()
+# def get_payroll_data(the_contract,houry_wage,from_date=None, to_date=None, deduction_percentage=0):
+	
 
-		working_hours = f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}"
+	# contract_workers = frappe.get_all(
+	# 	"Elitehr Supply Contract Workers",
+	# 	filters={
+	# 		"the_contract": the_contract
+	# 	},
+	# 	fields=["name"]
+	# )
+	# worker_list = [d.name for d in contract_workers]
+	# # frappe.log(worker_list)
 
-		total_hours_decimal = row["total_seconds"] / 3600
-		# daily_wage = total_hours_decimal * float(houry_wage)
+	# deduction_percentage = float(deduction_percentage or 0)
 
-		total = total_hours_decimal * float(houry_wage)
-		deduction = total * (deduction_percentage / 100)
-		net = total - deduction
+	# rows = frappe.get_all(
+	# 	"Elitehr Workers Check_in_out",
+	# 	filters={
+	# 		"the_worker":["in",worker_list],
+	# 		"date": ["between", [from_date, to_date]],
+	# 	},
+	# 	fields=["*"]
+	# )
 
-		total_total += total
-		total_deductions += deduction
-		total_net += net
+	# grouped = {}
 
-		payroll.append({
-			"worker": row["worker"],
-			"worker_name": row["worker_name"],
-			"days": len(row["days"]),
-			"working_hours": working_hours,
-			# "daily_wage": daily_wage,
-			"total": round(total, 2),
-			"deduction": round(deduction, 2),
-			"net": round(net, 2)
-		})
+	# for item in rows:
+	# 	worker = item.get("the_worker")
 
-	total_hours = int(total_seconds // 3600)
-	total_minutes = int((total_seconds % 3600) // 60)
+	# 	if worker not in grouped:
+	# 		grouped[worker] = {
+	# 			"worker": worker,
+	# 			"worker_name": item.get("worker_name"),
+	# 			"days": set(),
+	# 			"total_seconds": 0
+	# 		}
 
-	return {
-		"payroll": payroll,
-		"workers": len(payroll),
-		"total_seconds": total_seconds,
-		"hours": f"{str(total_hours).zfill(2)}:{str(total_minutes).zfill(2)}",
-		"total": round(total_total, 2),
-		"net": round(total_net, 2),
-		"total_deductions": round(total_deductions, 2)
-	}
+	# 	grouped[worker]["days"].add(str(item.get("date")))
+	# 	grouped[worker]["total_seconds"] += item.get("working_seconds") or 0
+
+	# payroll = []
+	# total_seconds = 0
+	# total_total = 0
+	# total_net = 0
+	# total_deductions = 0
+
+	# for row in grouped.values():
+	# 	total_seconds += row["total_seconds"]
+
+	# 	hours = int(row["total_seconds"] // 3600)
+	# 	minutes = int((row["total_seconds"] % 3600) // 60)
+
+	# 	working_hours = f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}"
+
+	# 	total_hours_decimal = row["total_seconds"] / 3600
+	# 	# daily_wage = total_hours_decimal * float(houry_wage)
+
+	# 	total = total_hours_decimal * float(houry_wage)
+	# 	deduction = total * (deduction_percentage / 100)
+	# 	net = total - deduction
+
+	# 	total_total += total
+	# 	total_deductions += deduction
+	# 	total_net += net
+
+	# 	payroll.append({
+	# 		"worker": row["worker"],
+	# 		"worker_name": row["worker_name"],
+	# 		"days": len(row["days"]),
+	# 		"working_hours": working_hours,
+	# 		# "daily_wage": daily_wage,
+	# 		"total": round(total, 2),
+	# 		"deduction": round(deduction, 2),
+	# 		"net": round(net, 2)
+	# 	})
+
+	# total_hours = int(total_seconds // 3600)
+	# total_minutes = int((total_seconds % 3600) // 60)
+
+	# return {
+	# 	"payroll": payroll,
+	# 	"workers": len(payroll),
+	# 	"total_seconds": total_seconds,
+	# 	"hours": f"{str(total_hours).zfill(2)}:{str(total_minutes).zfill(2)}",
+	# 	"total": round(total_total, 2),
+	# 	"net": round(total_net, 2),
+	# 	"total_deductions": round(total_deductions, 2)
+	# }
